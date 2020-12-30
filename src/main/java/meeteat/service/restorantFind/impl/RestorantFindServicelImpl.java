042ab660cfd7ec5a903d59ffff44611a1ecab1ea
lss7612 <lss76121@naver.com>
2020-12-30 10:16:52 +0900

    local change

--- src/main/java/meeteat/service/restorantFind/impl/RestorantFindServicelImpl.java
+++ src/main/java/meeteat/service/restorantFind/impl/RestorantFindServicelImpl.java
@@ -16,30 +16,30 @@ import meeteat.service.restorantFind.face.RestorantFindService;
 @Service
 public class RestorantFindServicelImpl implements RestorantFindService{
 
-	@Override
-	public String get(String apiURL, Map<String, String> requestHeaders) {
-		 HttpURLConnection con = connect(apiURL);
-	        try {
-	            con.setRequestMethod("GET");
-	            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
-	                con.setRequestProperty(header.getKey(), header.getValue());
-	            }
+   @Override
+   public String get(String apiURL, Map<String, String> requestHeaders) {
+       HttpURLConnection con = connect(apiURL);
+           try {
+               con.setRequestMethod("GET");
+               for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                   con.setRequestProperty(header.getKey(), header.getValue());
+               }
 
-	            int responseCode = con.getResponseCode();
-	            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
-	                return readBody(con.getInputStream());
-	            } else { // 에러 발생
-	                return readBody(con.getErrorStream());
-	            }
-	        } catch (IOException e) {
-	            throw new RuntimeException("API 요청과 응답 실패", e);
-	        } finally {
-	            con.disconnect();
-	        }
-	}
+               int responseCode = con.getResponseCode();
+               if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                   return readBody(con.getInputStream());
+               } else { // 에러 발생
+                   return readBody(con.getErrorStream());
+               }
+           } catch (IOException e) {
+               throw new RuntimeException("API 요청과 응답 실패", e);
+           } finally {
+               con.disconnect();
+           }
+   }
 
-	private HttpURLConnection connect(String apiURL) {
-		try {
+   private HttpURLConnection connect(String apiURL) {
+      try {
             URL url = new URL(apiURL);
             return (HttpURLConnection)url.openConnection();
         } catch (MalformedURLException e) {
@@ -47,14 +47,14 @@ public class RestorantFindServicelImpl implements RestorantFindService{
         } catch (IOException e) {
             throw new RuntimeException("연결이 실패했습니다. : " + apiURL, e);
         }
-	}
+   }
 
-	private String readBody(InputStream body) {
-		
-	   InputStreamReader streamReader = new InputStreamReader(body);
+   private String readBody(InputStream body) {
+      
+      InputStreamReader streamReader = new InputStreamReader(body);
 
         try (BufferedReader lineReader = new BufferedReader(streamReader)) {
-        	
+           
             StringBuilder responseBody = new StringBuilder();
 
             String line;
@@ -67,10 +67,10 @@ public class RestorantFindServicelImpl implements RestorantFindService{
         } catch (IOException e) {
             throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
         }
-		
-	}
-	
-	
-	
+      
+   }
+   
+   
+   
 
 }
