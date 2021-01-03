26263d386a5f60bad0f2b6a26855592897bd1f29
14051021@office.suwon.ac.kr <14051021@office.suwon.ac.kr>
2021-01-03 21:55:45 +0900

    autowrite bug fix

--- src/main/java/meeteat/service/restorantFind/face/RestorantFindService.java
+++
@@ -1,9 +0,0 @@
-package meeteat.service.restorantFind.face;
-
-import java.util.Map;
-
-public interface RestorantFindService {
-
-	String get(String apiURL, Map<String, String> requestHeaders);
-
-}
--- src/main/webapp/resources/js/report/doReport.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#report_content").focus();
-})
-
-function popupClose(){
-	console.log("클릭");
-	window.open('','_self').close();
-
-}
